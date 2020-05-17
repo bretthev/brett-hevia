64ecb97f9c9afec2f08d789d764161975f00298f
Brett Hevia <brett.hevia@gmail.com>
2020-05-16 20:18:20 -0700

    first commit, setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {NextPage} from "next";
+
+const Home: NextPage<any> = () => {
+  return (
+    <>
+      <h1>Welcome to Brett's website.</h1>
+    </>
+  );
+};
+
+export default Home;
